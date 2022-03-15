cbf494d2a8d773cd921adb45cfdde05359d862e1
Juan Carlos Asensio <jasensio@ufm.edu>
2022-03-14 20:51:57 -0400

    Initial React/Redux setup

---
+++ src/store/configureStore.js
@@ -0,0 +1,30 @@
+import {createStore, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "../reducers/rootReducer";
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+
+export default function configureStore(preloadedState) {
+    const middlewares = [thunk];
+    const middlewareEnhancer = applyMiddleware(...middlewares);
+
+    const storeEnhancers = [middlewareEnhancer];
+    
+    const composedEnhancer = composeWithDevTools(...storeEnhancers);
+
+    const store = createStore(
+        rootReducer,
+        preloadedState,
+        composedEnhancer
+    );
+
+    if (process.env.NODE_ENV !== "production") {
+      if (module.hot) {
+        module.hot.accept("../reducers/rootReducer", () => {
+          const newRootReducer = require("../reducers/rootReducer").default;
+          store.replaceReducer(newRootReducer)
+        });
+      };
+    };
+
+    return store;
+}
